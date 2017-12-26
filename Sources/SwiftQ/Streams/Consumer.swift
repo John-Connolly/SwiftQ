0af6a00be0c10b51f1a5721312658760e200fdc4
John Connolly <connoljo2@gmail.com>
2017-12-26 15:26:32 -0400

    Changes to the DecoderStream

--- Sources/SwiftQ/Streams/Consumer.swift
+++ Sources/SwiftQ/Streams/Consumer.swift
@@ -8,109 +8,6 @@
 import Foundation
 import Async
 import Redis
-//import SwiftRedis
-//
-//public final class Consumer: Async.OutputStream  {
-//    
-//    public typealias Output = Data
-//    
-//    private var outputStream: BasicStream<Output> = .init()
-//    
-//    let decoder: DecoderStream
-//    
-//
-//    public init(types: [Task.Type]) {
-//        self.decoder = DecoderStream(types)
-//    }
-//    
-//    let eventLoops = (1...4).map { num -> BlockingStream in
-//        let eventLoop = DispatchQueue(label: "worker.eventloop.\(num)",qos: .userInitiated)
-//        return BlockingStream(on: eventLoop)
-//    }
-//
-//    
-//    public func run() {
-//
-//        for eventLoop in eventLoops {
-//            eventLoop.run()
-//        }
-//        
-//        let group = DispatchGroup()
-//        group.enter()
-//        group.wait()
-//        
-//        //        RunLoop.main.run()
-//        //        self.stream(to: decoder).drain { task in
-//        ////            print(task)
-//        //
-//        //            }.catch { error in
-//        //              print(error)
-//        //        }
-
-
-
-
-//final class BlockingStream: Async.OutputStream {
-//    
-//    
-//    /// See OutputStream.Output
-//    typealias Output = RedisData
-//    
-//    var outputStream: BasicStream<Output> = .init()
-//    
-//    
-//    let eventLoop: EventLoop
-//    let pool: BlockedClientPool
-//    let semaphore = DispatchSemaphore(value: 50)
-//    
-//    
-//    init(on eventLoop: EventLoop) {
-//        self.eventLoop = eventLoop
-//        self.pool = BlockedClientPool(on: eventLoop, clients: 50)
-//    }
-//    
-//    
-//    func run() {
-//        
-//        repeat {
-//            self.semaphore.wait()
-//            
-//            let data = self.pool.retain { client -> Future<RedisData> in
-//                return client.run(command: "BRPOPLPUSH", arguments: ["myList","newList","0"])
-//                
-//            }
-//            
-//            data.always {
-//                self.semaphore.signal()
-//            }
-//        } while true
-//    }
-//    
-//
-//
-//public func onError(_ error: Error) {
-//    outputStream.onError(error)
-//}
-//
-//
-//public func onOutput<I>(_ input: I) where I: Async.InputStream, Output == I.Input {
-//    outputStream.onOutput(input)
-//}
-//
-//
-//func close() {
-//    outputStream.close()
-//}
-//
-//
-//func onClose(_ onClose: ClosableStream) {
-//    outputStream.onClose(onClose)
-//}
-//
-//}
-//
-//
-//
 
 //final class CompletionStream {
 //final class WorkStream: Async.Stream  {
@@ -125,7 +22,13 @@ final class Consumer {
         return eventLoop
     }
     
-    init() { }
+    
+    private let decoder: DecoderStream
+    
+    init() {
+        self.decoder = DecoderStream([]) // GET Types From config
+        //        let _ = self.stream(to: decoder)
+    }
     
     
     
@@ -136,10 +39,8 @@ public final class DataStream: Async.OutputStream, Async.ConnectionContext {
     
     public typealias Output = RedisData
     
-    let eventLoop: EventLoop
-    let client: RedisClient
-    let decoder: DecoderStream
-    
+    private let eventLoop: EventLoop
+    private let client: RedisClient
     
     private var downstream: AnyInputStream<Output>?
     
@@ -149,16 +50,17 @@ public final class DataStream: Async.OutputStream, Async.ConnectionContext {
     public init(on eventLoop: EventLoop) {
         self.eventLoop = eventLoop
         self.client = try! RedisClient.connect(on: eventLoop)
-        self.decoder = DecoderStream()
-        let _ = self.stream(to: decoder)
     }
     
-    func accept() {
+    private func accept() {
         eventLoop.async {
             
             self.client.run(command: "BRPOPLPUSH", arguments: ["myList","newList","0"]).do { data in
                 self.downstream?.next(data)
-                }.catch { _ in }
+                
+                }.catch { error in
+                    self.downstream?.error(error)
+            }
             
         }
         
@@ -177,7 +79,11 @@ public final class DataStream: Async.OutputStream, Async.ConnectionContext {
         accept()
     }
     
-    func cancel() { }
+    // Nothing really to clean up.
+    func cancel() {
+        print("Data Stream Connection Canceled.")
+        
+    }
     
     public func output<S>(to inputStream: S) where S: Async.InputStream, S.Input == Output {
         downstream = AnyInputStream(inputStream)
@@ -186,69 +92,5 @@ public final class DataStream: Async.OutputStream, Async.ConnectionContext {
     
     
 }
-
-final class DecoderStream: Async.Stream, Async.ConnectionContext {
-    
-    typealias Input = RedisData
-    
-    typealias Output = Task
-    
-    /// The upstream providing byte buffers
-    var upstream: ConnectionContext?
-    
-    /// Use a basic output stream to implement server output stream.
-    var downstream: AnyInputStream<Output>?
-    
-    /// Remaining downstream demand
-    var downstreamDemand: UInt = 0
-    
-    var input: RedisData? {
-        didSet {
-            
-        }
-    }
-    
-    
-    func input(_ event: InputEvent<RedisData>) {
-        switch event {
-        case .close:
-            downstream?.close()
-        case .connect(let upstream):
-            self.upstream = upstream
-        case .error(let error):
-            downstream?.error(error)
-        case .next(let next):
-            print(next)
-            //              self.downstream?.error(error)
-        }
-    }
-    
-    func connection(_ event: ConnectionEvent) {
-        switch event {
-        case .cancel:
-            self.downstreamDemand = 0
-        case .request(let demand):
-            self.downstreamDemand += demand
-        }
-        
-        guard downstreamDemand > 0, input != nil else {
-            upstream?.request()
-            return
-        }
-        
-        //        do {
-        //            try transform()
-        //        } catch {
-        //            self.downstream?.error(error)
-        //        }
-    }
-    
-    func output<S>(to inputStream: S) where S : Async.InputStream, Output == S.Input {
-        self.downstream = AnyInputStream(inputStream)
-        inputStream.connect(to: self)
-    }
-    
-}
-
 
 
--- Sources/SwiftQ/Streams/DecoderStream.swift
+++ Sources/SwiftQ/Streams/DecoderStream.swift
@@ -8,15 +8,18 @@
 import Foundation
 import Async
 
-/*
-final class DecoderStream: Async.Stream {
+final class DecoderStream: Async.Stream, Async.ConnectionContext {
     
-    typealias Input = Foundation.Data
+    typealias Input = Data
     
-    /// See OutputStream.Output
     typealias Output = Task
     
-    var outputStream: BasicStream<Output> = .init()
+    var upstream: ConnectionContext?
+    
+    var downstream: AnyInputStream<Output>?
+    
+    /// Remaining downstream demand
+    var downstreamDemand: UInt = 0
     
     private let resources: [InitResource]
     
@@ -24,14 +27,55 @@ final class DecoderStream: Async.Stream {
         self.resources = types.map(InitResource.init)
     }
     
-    public func onInput(_ input: Foundation.Data) {
-        do {
-            let taskName = try input.jsonDictionary(key: String.self, value: Any.self).taskName()
-            let task = try decode(task: input, with: taskName)
-            outputStream.onInput(task)
-        } catch {
-            onError(error)
+    func input(_ event: InputEvent<Data>) {
+        switch event {
+        case .close:
+            downstream?.close()
+        case .connect(let upstream):
+            self.upstream = upstream
+        case .error(let error):
+            downstream?.error(error)
+        case .next(let next):
+            
+            do {
+                let task = try transform(next)
+                downstream?.next(task)
+            } catch {
+                self.downstream?.error(error)
+            }
+            
         }
+    }
+    
+    func connection(_ event: ConnectionEvent) {
+        switch event {
+        case .cancel:
+            self.downstreamDemand = 0
+        case .request(let demand):
+            self.downstreamDemand += demand
+        }
+        
+        guard downstreamDemand > 0 else {
+            upstream?.request()
+            return
+        }
+        // Not sure what to do here.
+        //        do {
+        //            try transform()
+        //        } catch {
+        //            self.downstream?.error(error)
+        //        }
+    }
+    
+    
+    func output<S>(to inputStream: S) where S : Async.InputStream, Output == S.Input {
+        self.downstream = AnyInputStream(inputStream)
+        inputStream.connect(to: self)
+    }
+    
+    private func transform(_ input: Foundation.Data) throws -> Task {
+        let taskName = try input.jsonDictionary(key: String.self, value: Any.self).taskName()
+        return try decode(task: input, with: taskName)
     }
     
     private func decode(task: Data, with name: String) throws -> Task {
@@ -44,25 +88,4 @@ final class DecoderStream: Async.Stream {
             .or(throw:  SwiftQError.taskNotFound)
     }
     
-    public func onError(_ error: Error) {
-        outputStream.onError(error)
-    }
-    
-    
-    public func onOutput<I>(_ input: I) where I: Async.InputStream, Output == I.Input {
-        outputStream.onOutput(input)
-    }
-    
-    
-    func close() {
-        outputStream.close()
-    }
-    
-    
-    func onClose(_ onClose: ClosableStream) {
-        outputStream.onClose(onClose)
-    }
-    
-    
 }
-*/
