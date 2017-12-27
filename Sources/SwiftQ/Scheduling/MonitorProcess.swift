a6a7b7cb61305af92a334cdbed7416281bf26554
John Connolly <connoljo2@gmail.com>
2017-12-27 15:30:54 -0400

    Changes to MonitorProcess

--- Sources/SwiftQ/Scheduling/MonitorProcess.swift
+++ Sources/SwiftQ/Scheduling/MonitorProcess.swift
@@ -11,10 +11,32 @@ struct MonitorProcess: Process {
     
     let repeating = DispatchTimeInterval.seconds(1)
     
+    //TODO: Pass in name
     func event(container: Container) {
-          _ = container.get(RedisContainer.self)?.client
+        guard let client = container.get(RedisContainer.self)?.client else {
+            return
+        }
+        
+        let command = Command.zrangebyscore(key: "name", min: "-inf", max: Date().unixTime.description)
+        let data = client.execute(command: command).map(to: [Data].self) { resp in
+            return try resp.array.or(throw: SwiftQError.noValue)
+        }
+        
+        data.do { data in
+            
+            guard data.count > 0 else {
+                return
+            }
+            
+            // Transfer to work queue.
+            
+            
+            }.catch { error in
+                Logger.log(error, level: .warning)
+        }
         
         print("working")
     }
+    
     
 }
