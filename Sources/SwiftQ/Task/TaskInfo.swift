0abb9af9e9c7a701d4eb37913dd9cb384a0a79fe
John Connolly <connoljo2@gmail.com>
2018-12-29 16:43:24 -0400

    Add taskInfo

--- Sources/SwiftQ/Task/TaskInfo.swift
+++ Sources/SwiftQ/Task/TaskInfo.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct TaskInfo<T: Task>: InfoType {
+    let task: T
+    let uuid: String
+    let name: String
+    var enqueuedAt: Int
+    var retryCount = 0
+    var taskType: TaskType
+
+    init(_ task: T) {
+        self.name = String(describing: T.self)
+        self.task = task
+        self.uuid = UUID().uuidString
+        self.taskType = .task
+        self.enqueuedAt = Date().unixTime
+    }
+
+}
+
+protocol InfoType: Codable {
+
+}
+
+extension InfoType {
+    init(data: Data) throws {
+        //        TaskInfo<Email.self>
+        self = try JSONDecoder().decode(Self.self, from: data)
+    }
+}
